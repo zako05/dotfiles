4f267b2043789395af0bec3f617ffbfa7cfb3821
Michal Zak <zako05@gmail.com>
2023-07-12 11:31:13 +0200

    remove start tmux window with index 1, and remove window index 0 in dev_env script, after all windows setup in each session

--- scripts/dev_env.sh
+++ scripts/dev_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-session=(workspace vestberry)
+session=(workspace vectary vestberry)
 
 get_session () {
   get_session_name=$(tmux ls | grep $1 | awk '{print $1}')
@@ -14,29 +14,51 @@ get_session () {
   fi
 }
 
+# Gets rid of window 0, which is not accessible right away (hence sleep 1)
+no_window_zero () {
+  sleep 1
+  tmux unlink-window -k -t $1:0
+}
+
 set_session () {
   workspace=$HOME/$1
   if [[ "$1" == "workspace" ]] 
   then
-    tmux new-window -t "${1}:2" -n articles -c "${workspace}/article-draft" -d
-    tmux new-window -t "${1}:3" -n vitepress-articles -c "${workspace}/vitepress-articles" -d
-    tmux new-window -t "${1}:4" -n jsbootcamp -c "${workspace}/js-bootcamp" -d \; split-window -t jsbootcamp -h -c "${workspace}/js-bootcamp" 
-    tmux new-window -t "${1}:5" -n testing-javascript -c "${workspace}/testing-javascript" -d
+    tmux new-window -t $1:1 -n articles -c "${workspace}/article-draft" -d
+    tmux new-window -t $1:2 -n vitepress-articles -c "${workspace}/vitepress-articles" -d
+    tmux new-window -t $1:3 -n jsbootcamp -c "${workspace}/js-bootcamp" -d
+      tmux split-window -t jsbootcamp -h -c "${workspace}/js-bootcamp" 
+      tmux split-window -t jsbootcamp -c "${workspace}/js-bootcamp" \; select-pane -t 1
+    tmux new-window -t $1:4 -n testing-javascript -c "${workspace}/testing-javascript" -d
       tmux split-window -t testing-javascript -h -c "${workspace}/testing-javascript"
       tmux split-window -t testing-javascript -c "${workspace}/testing-javascript" \; select-pane -t 1 
-    tmux new-window -t "${1}:6" -n tau -c "${workspace}/tau/playwright-js-basics" -d
-    tmux new-window -t "${1}:7" -n cy-tips -c "${workspace}/cypress-tips" -d \; split-window -t cy-tips -h -c "${workspace}/cypress-tips" 
-    tmux new-window -t "${1}:8" -n 10-days-of-js -c "${workspace}/10-days-of-js" -d \; split-window -t 10-days-of-js -h -c "${workspace}/10-days-of-js" 
-    tmux new-window -t "${1}:9" -n vue-realworld-example -c "${workspace}/vue3-realworld-example-app" -d \; split-window -t vue-realworld-example -h -c "${workspace}/vue3-realworld-example-app" 
+    tmux new-window -t $1:5 -n tau -c "${workspace}/tau" -d
+    tmux new-window -t $1:6 -n cy-tips -c "${workspace}/cypress-tips" -d \; split-window -t cy-tips -h -c "${workspace}/cypress-tips" 
+    tmux new-window -t $1:7 -n 10-days-of-js -c "${workspace}/10-days-of-js" -d \; split-window -t 10-days-of-js -h -c "${workspace}/10-days-of-js" 
+    tmux new-window -t $1:8 -n vue-realworld-example -c "${workspace}/vue3-realworld-example-app" -d \; split-window -t vue-realworld-example -h -c "${workspace}/vue3-realworld-example-app" 
+    no_window_zero $1
+  fi
+  if [[ "$1" == "vectary" ]] 
+  then
+    tmux new-window -t $1:1 -n monorepo -c "${workspace}/vctr-monorepo" -d
+      tmux split-window -t monorepo -h -c "${workspace}/vctr-monorepo" \; select-pane -t 1
+    tmux new-window -t $1:2 -n specs -c "${workspace}/vctr-monorepo/packages/@vctr-tools/vctr-tests" -d
+      tmux split-window -t specs -h -c "${workspace}/vctr-monorepo/packages/@vctr-tools/vctr-tests" -d
+    tmux new-window -t $1:3 -n testing -c "${workspace}/todos" -d
+      tmux split-window -t testing -h -c "${workspace}/tcs"
+      tmux split-window -t testing -c "${workspace}/bugs" \; select-pane -t 1
+      tmux split-window -t testing -c "${workspace}/tmp"
+    no_window_zero $1
   fi
   if [[ "$1" == "vestberry" ]] 
   then
-    tmux new-window -t "${1}:2" -n vbr-client-server -c "${workspace}/vestberry" -d
+    tmux new-window -t $1:1 -n vbr-client-server -c "${workspace}/vestberry" -d
       tmux split-window -t vbr-client-server -h -c "${workspace}/vestberry"
       tmux split-window -t vbr-client-server -c "${workspace}/vestberry" \; select-pane -t 1
       tmux split-window -t vbr-client-server -c "${workspace}/vestberry"
-    tmux new-window -t "${1}:3" -n cy-run -c "${workspace}/vestberry" -d
-    tmux new-window -t "${1}:4" -n cy-specs -c "${workspace}/vestberry" -d
+    tmux new-window -t $1:2 -n cy-run -c "${workspace}/vestberry" -d
+    tmux new-window -t $1:3 -n cy-specs -c "${workspace}/vestberry" -d
+    no_window_zero $1
   fi
 }
 
@@ -45,8 +67,9 @@ for s in "${session[@]}"
 do
   if get_session $s
   then
-    tmux new-session -s $s -n default -c  $HOME/$s -d
-    echo "Session $s has been created successfully."
+    # tmux new-session -d -s $s -n default -c $HOME/$s
+    tmux new-session -d -s $s
     set_session $s
+    echo "Session $s has been created successfully."
   fi
 done
