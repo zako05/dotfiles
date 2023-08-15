cbe6e7987def158957ecd20d2aa225ad89a66c52
Michal Zak <zako05@gmail.com>
2023-08-15 10:14:10 +0200

    remove vestberry & vectary workspaces from dev env

--- scripts/dev_env.sh
+++ scripts/dev_env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-session=(workspace vectary vestberry)
+session=(workspace instea)
 
 get_session () {
   get_session_name=$(tmux ls | grep $1 | awk '{print $1}')
@@ -36,28 +36,13 @@ set_session () {
     tmux new-window -t $1:6 -n cy-tips -c "${workspace}/cypress-tips" -d \; split-window -t cy-tips -h -c "${workspace}/cypress-tips" 
     tmux new-window -t $1:7 -n 10-days-of-js -c "${workspace}/10-days-of-js" -d \; split-window -t 10-days-of-js -h -c "${workspace}/10-days-of-js" 
     tmux new-window -t $1:8 -n vue-realworld-example -c "${workspace}/vue3-realworld-example-app" -d \; split-window -t vue-realworld-example -h -c "${workspace}/vue3-realworld-example-app" 
+    tmux new-window -t $1:9 -n instea -c "${workspace}/todos" -d
     no_window_zero $1
   fi
-  if [[ "$1" == "vectary" ]] 
+  if [[ "$1" == "instea" ]] 
   then
-    tmux new-window -t $1:1 -n monorepo -c "${workspace}/vctr-monorepo" -d
-      tmux split-window -t monorepo -h -c "${workspace}/vctr-monorepo" \; select-pane -t 1
-    tmux new-window -t $1:2 -n specs -c "${workspace}/vctr-monorepo/packages/@vctr-tools/vctr-tests" -d
-      tmux split-window -t specs -h -c "${workspace}/vctr-monorepo/packages/@vctr-tools/vctr-tests" -d
-    tmux new-window -t $1:3 -n testing -c "${workspace}/todos" -d
-      tmux split-window -t testing -h -c "${workspace}/tcs"
-      tmux split-window -t testing -c "${workspace}/bugs" \; select-pane -t 1
-      tmux split-window -t testing -c "${workspace}/tmp"
-    no_window_zero $1
-  fi
-  if [[ "$1" == "vestberry" ]] 
-  then
-    tmux new-window -t $1:1 -n vbr-client-server -c "${workspace}/vestberry" -d
-      tmux split-window -t vbr-client-server -h -c "${workspace}/vestberry"
-      tmux split-window -t vbr-client-server -c "${workspace}/vestberry" \; select-pane -t 1
-      tmux split-window -t vbr-client-server -c "${workspace}/vestberry"
-    tmux new-window -t $1:2 -n cy-run -c "${workspace}/vestberry" -d
-    tmux new-window -t $1:3 -n cy-specs -c "${workspace}/vestberry" -d
+    tmux new-window -t $1:1 -n instea -c "${workspace}" -d
+    tmux new-window -t $1:2 -n quick -c "${workspace}/quick" -d
     no_window_zero $1
   fi
 }
